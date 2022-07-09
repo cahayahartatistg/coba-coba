0946584cc4c5c7fc7d0280ab78bbff27bcb04e34
Rozin Dzakwan <rozindzakwan@gmail.com>
2022-07-09 14:36:28 +0700

    coba

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import ReactDOM  from "react-dom/client";
+
+
+function Welcome(props){
+    return <h1>Selamat Datang {props.nama}</h1>
+}
+
+let nama ;
+
+function Home(){
+if(nama){
+    return <Welcome nama = {nama} />
+}else{
+    return <button>Login</button>
+}
+
+    
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<Home/>)
+
